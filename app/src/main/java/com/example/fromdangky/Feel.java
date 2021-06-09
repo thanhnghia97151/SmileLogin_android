6eb32d2818c78ef66eac9a87ed181371e86af186
thanhnghia97151 <nguyenthanhnghia0907@gmail.com>
2021-06-09 10:31:04 +0700

    smile

---
+++ app/src/main/java/com/example/fromdangky/Feel.java
@@ -0,0 +1,24 @@
+package com.example.fromdangky;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Feel extends AppCompatActivity {
+    Button btnfinish;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feel);
+        btnfinish = findViewById(R.id.btnfinish);
+        btnfinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
